bcd1431bd10380679b4d0ac091a517aa209b814e
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-03 08:57:24 -0500

    2A done
    Naive, brute force all the way!

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,8 +1,38 @@
 defmodule AdventOfCode2025.Day02 do
   alias AdventOfCode2025.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    ranges = lines |> parse_input()
+
+    ranges
+    |> Stream.concat()
+    |> Stream.filter(&invalid?/1)
+    |> Enum.sum()
+  end
+
+  defp parse_input(lines) do
+    Enum.join(lines, "")
+    |> String.split(",")
+    |> Enum.map(fn range_string ->
+      [a, b] =
+        range_string
+        |> String.split("-")
+        |> Enum.map(&String.to_integer/1)
+
+      a..b
+    end)
+  end
+
+  defp invalid?(num) do
+    num_string = Integer.to_string(num)
+    size = String.length(num_string)
+
+    if Integer.mod(size, 2) == 1 do
+      false
+    else
+      {front, back} = String.split_at(num_string, Integer.floor_div(size, 2))
+      front == back
+    end
   end
 
   def part_b(_lines) do
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -3,14 +3,15 @@ defmodule AdventOfCode2025.Day02Test do
 
   alias AdventOfCode2025.Day02
 
-  @tag :skip
   test "Day02 part A example" do
-    assert Day02.part_a(example_input()) == 42
+    assert Day02.part_a(example_input()) == 1_227_775_554
   end
 
   defp example_input() do
     """
-    TODO
+    11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+    1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+    824824821-824824827,2121212118-2121212124
     """
     |> String.trim()
     |> String.split("\n")
