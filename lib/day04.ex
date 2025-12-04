0d98df9527862929855c78524413663d4ccc32ed
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-04 18:13:20 -0500

    Day04A done

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,8 +1,24 @@
 defmodule AdventOfCode2025.Day04 do
   alias AdventOfCode2025.Helpers
+  alias AdventOfCode2025.Grid
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    points = Grid.parse_string_grid(lines) |> Map.get("@")
+
+    points
+    |> Enum.map(&count_neighbors(&1, points))
+    |> Enum.filter(fn {_point, point_count} -> point_count < 4 end)
+    |> Enum.count()
+  end
+
+  defp count_neighbors(point, points) do
+    point_count =
+      point
+      |> Grid.candidate_neighbors_with_diagonals()
+      |> Enum.filter(&MapSet.member?(points, &1))
+      |> Enum.count()
+
+    {point, point_count}
   end
 
   def part_b(_lines) do
--- lib/grid.ex
+++ lib/grid.ex
@@ -56,4 +56,17 @@ defmodule AdventOfCode2025.Grid do
   def candidate_neighbors({x, y}) do
     MapSet.new([{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}])
   end
+
+  def candidate_neighbors_with_diagonals({x, y}) do
+    MapSet.new([
+      {x - 1, y},
+      {x + 1, y},
+      {x, y - 1},
+      {x, y + 1},
+      {x + 1, y + 1},
+      {x - 1, y + 1},
+      {x + 1, y - 1},
+      {x - 1, y - 1}
+    ])
+  end
 end
