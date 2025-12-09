1ae8a8af555560d0b1cc27cc7e896856e4f64522
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-09 07:09:14 -0500

    Day 09A done

--- lib/day09.ex
+++ lib/day09.ex
@@ -1,8 +1,28 @@
 defmodule AdventOfCode2025.Day09 do
   alias AdventOfCode2025.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    lines
+    |> parse_input()
+    |> Helpers.pairs()
+    |> Enum.map(&rectangle_size/1)
+    |> Enum.max()
+  end
+
+  defp parse_input(lines) do
+    Enum.map(lines, fn line ->
+      line
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
+  defp rectangle_size({{x0, y0}, {x1, y1}}) do
+    dx = abs(x1 - x0) + 1
+    dy = abs(y1 - y0) + 1
+
+    dx * dy
   end
 
   def part_b(_lines) do
