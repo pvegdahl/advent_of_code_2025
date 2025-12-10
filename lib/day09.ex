d7806b943c4ca96ed2f586b2938e4254a9e50410
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-09 21:33:42 -0500

    Some intermetiate stuff is passing
    This one is hard

--- lib/day09.ex
+++ lib/day09.ex
@@ -29,6 +29,48 @@ defmodule AdventOfCode2025.Day09 do
     -1
   end
 
+  def contains?(polygon, {{x0, y0}, {x1, y1}} = _rectangle) do
+    {min_x, max_x} = polygon |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+    {min_y, max_y} = polygon |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+
+    # So dumb!
+    x1 < max_x
+  end
+
+  def point_in_polygon?(polygon, point, max_x) do
+    verticals = vertical_line_map(polygon)
+
+    if intersects_vertical?(verticals, point) do
+      IO.inspect(point, label: "point")
+      true
+    else
+      intersections = count_intersections(verticals, point, max_x)
+      Integer.mod(intersections, 2) == 1
+    end
+
+  end
+
+  defp intersects_vertical?(verticals, {x, y}) do
+    case Map.get(verticals, x) do
+      nil -> false
+      range -> y in range
+    end
+  end
+
+  defp count_intersections(verticals, {x, y}, max_x) do
+    Enum.count(x..max_x, fn specific_x -> intersects_vertical?(verticals, {specific_x, y}) end) |> IO.inspect()
+  end
+
+  defp vertical_line_map(polygon) do
+    polygon
+    |> Enum.chunk_every(2, 1, Enum.take(polygon, 1))
+    |> Enum.filter(fn [{x0, _y0}, {x1, _y1}] -> x0 == x1 end)
+    |> Map.new(fn [{x, y0}, {x, y1}] ->
+      [y00, y01] = Enum.sort([y0, y1])
+      {x, y00..y01}
+    end)
+  end
+
   def a() do
     Helpers.file_to_lines!("inputs/day09.txt")
     |> part_a()
--- test/day09_test.exs
+++ test/day09_test.exs
@@ -23,8 +23,37 @@ defmodule AdventOfCode2025.Day09Test do
     |> Enum.map(&String.trim/1)
   end
 
-  @tag :skip
   test "Day09 part B example" do
-    assert Day09.part_b(example_input()) == 42
+    assert Day09.part_b(example_input()) == 24
+  end
+
+  describe "contains?/2" do
+    test "easily contained" do
+      polygon = [{0, 0}, {10, 0}, {10, 10}, {0, 10}]
+      rectangle = {{2, 2}, {6, 6}}
+
+      assert Day09.contains?(polygon, rectangle)
+    end
+
+    test "easily not contained" do
+      polygon = [{0, 0}, {10, 0}, {10, 10}, {0, 10}]
+      rectangle = {{12, 12}, {16, 16}}
+
+      refute Day09.contains?(polygon, rectangle)
+    end
+  end
+
+  describe "point_in_polygon?/2" do
+    test "simple inside" do
+      polygon = [{0, 0}, {10, 0}, {10, 10}, {0, 10}]
+
+      assert Day09.point_in_polygon?(polygon, {5, 5}, 10)
+    end
+
+    test "outside the U" do
+      polygon = [{0, 0}, {10, 0}, {10, 10}, {8, 10}, {8, 2}, {2, 2}, {2, 10}, {0, 10}]
+
+      refute Day09.point_in_polygon?(polygon, {5, 5}, 10)
+    end
   end
 end
