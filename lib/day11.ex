6be66b1510e305cdd77fe87068a97d29e52cbf08
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-14 09:10:04 -0500

    11B passes tests, but is too slow
    Time for memoization

--- lib/day11.ex
+++ lib/day11.ex
@@ -26,8 +26,32 @@ defmodule AdventOfCode2025.Day11 do
     |> Enum.sum()
   end
 
-  def part_b(_lines) do
-    -1
+  def part_b(lines) do
+    lines
+    |> parse_input()
+    |> count_paths_with_required_nodes("svr", "out", ["dac", "fft"])
+    |> Map.get([])
+  end
+
+  defp count_paths_with_required_nodes(_graph, destination, destination, required_nodes), do: %{required_nodes => 1}
+
+  defp count_paths_with_required_nodes(graph, start, destination, required_nodes) do
+    graph
+    |> Map.get(start)
+    |> Enum.flat_map(fn node ->
+      intermediate_result = count_paths_with_required_nodes(graph, node, destination, required_nodes)
+
+      if node in required_nodes do
+        Enum.map(intermediate_result, fn {still_required_nodes, count} ->
+          {List.delete(still_required_nodes, node), count}
+        end)
+      else
+        Map.to_list(intermediate_result)
+      end
+    end)
+    |> Enum.reduce(%{}, fn {key, value}, acc ->
+      Map.update(acc, key, value, fn existing_value -> existing_value + value end)
+    end)
   end
 
   def a() do
--- test/day11_test.exs
+++ test/day11_test.exs
@@ -4,10 +4,10 @@ defmodule AdventOfCode2025.Day11Test do
   alias AdventOfCode2025.Day11
 
   test "Day11 part A example" do
-    assert Day11.part_a(example_input()) == 5
+    assert Day11.part_a(example_input_a()) == 5
   end
 
-  defp example_input() do
+  defp example_input_a() do
     """
     aaa: you hhh
     you: bbb ccc
@@ -26,8 +26,8 @@ defmodule AdventOfCode2025.Day11Test do
   end
 
   describe "parse_input/1" do
-    test "example_input" do
-      assert Day11.parse_input(example_input()) == %{
+    test "example_input_a" do
+      assert Day11.parse_input(example_input_a()) == %{
                "aaa" => ["you", "hhh"],
                "you" => ["bbb", "ccc"],
                "bbb" => ["ddd", "eee"],
@@ -42,8 +42,28 @@ defmodule AdventOfCode2025.Day11Test do
     end
   end
 
-  @tag :skip
   test "Day11 part B example" do
-    assert Day11.part_b(example_input()) == 42
+    assert Day11.part_b(example_input_b()) == 2
+  end
+
+  defp example_input_b() do
+    """
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out
+    """
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
   end
 end
