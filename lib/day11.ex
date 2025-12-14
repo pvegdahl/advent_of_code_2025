48ddaa200645ff53b5525dd956f9f67d8fbee741
Philip Vegdahl <pvegdahl@gmail.com>
2025-12-14 07:21:44 -0500

    11A done
    Surprisingly, it did not require memoization to be performant. I wonder
    if I'll need that for part B.

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,8 +1,10 @@
 defmodule AdventOfCode2025.Day11 do
   alias AdventOfCode2025.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    lines
+    |> parse_input()
+    |> count_paths("you", "out")
   end
 
   def parse_input(lines) do
@@ -13,6 +15,15 @@ defmodule AdventOfCode2025.Day11 do
       {key, values}
     end)
     |> Map.new()
+  end
+
+  defp count_paths(_graph, destination, destination), do: 1
+
+  defp count_paths(graph, start, destination) do
+    graph
+    |> Map.get(start)
+    |> Enum.map(fn node -> count_paths(graph, node, destination) end)
+    |> Enum.sum()
   end
 
   def part_b(_lines) do
